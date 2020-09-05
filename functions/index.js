cc0a4b4eeb061e81e04d77ebff05894ef540cf8e
Patrizio Bekerle <patrizio@bekerle.com>
2020-09-05 14:24:25 +0200

    Add dummy firebase function

---
+++ functions/index.js
@@ -0,0 +1,21 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
+
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+app.get('/api/bong', (req, res) => {
+    res.set('Cache-Control', 'public, max-age=300, s-maxage=400')
+    const date = new Date();
+    const hours = (date.getHours() % 12) + 2;  // London is UTC + 1hr;
+    res.json({bongs: 'BONG '.repeat(hours)});
+  });
+
+exports.app = functions.https.onRequest(app);
