1c2d5234274956678f63b300159043c29040bc15
Patrizio Bekerle <patrizio@bekerle.com>
2020-09-08 22:20:52 +0200

    Fixed exceljs for node.js 8

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,19 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const express = require('express');
 const bearerToken = require('express-bearer-token');
-const ExcelJS = require('exceljs');
+// const ExcelJS = require('exceljs');
+
+// polyfills required by exceljs for node.js 8
+// https://github.com/exceljs/exceljs#es5-imports
+require('core-js/modules/es.promise');
+require('core-js/modules/es.string.includes');
+require('core-js/modules/es.object.assign');
+require('core-js/modules/es.object.keys');
+require('core-js/modules/es.symbol');
+require('core-js/modules/es.symbol.async-iterator');
+require('regenerator-runtime/runtime');
+
+const ExcelJS = require('exceljs/dist/es5');
 const cors = require('cors');
 const app = express();
 const corsOptions = {
