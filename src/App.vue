191a02f5c78ccde12e7fff8e9378adda8742f567
Patrizio Bekerle <patrizio@bekerle.com>
2020-09-08 22:41:59 +0200

    Change toolbar title

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@
         clipped-left
       >
         <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-toolbar-title>Application</v-toolbar-title>
+        <v-toolbar-title>Kages Controlling</v-toolbar-title>
       </v-app-bar>
 
       <v-main>
