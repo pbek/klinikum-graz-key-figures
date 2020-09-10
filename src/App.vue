661c45b7c2a0980b624f311afa8324b937680071
Patrizio Bekerle <patrizio@bekerle.com>
2020-09-10 22:41:44 +0200

    Remove duplicate v-app

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,48 @@
 <template>
   <v-app id="inspire">
-    <v-app id="inspire">
-      <v-navigation-drawer
-        v-model="drawer"
-        app
-        clipped
-      >
-        <v-list dense>
-          <v-list-item link v-if="user !== null" @click="logout">
-            <v-list-item-action>
-              <v-icon>mdi-logout</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>Logout {{user.email}}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item link v-if="user !== null" @click="logout">
+          <v-list-item-action>
+            <v-icon>mdi-logout</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Logout {{user.email}}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-      <v-app-bar
-        app
-        clipped-left
-      >
-        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-toolbar-title>{{title}}</v-toolbar-title>
-      </v-app-bar>
+    <v-app-bar
+      app
+      clipped-left
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>{{title}}</v-toolbar-title>
+    </v-app-bar>
 
-      <v-main>
-        <v-container
-          class="fill-height"
-          fluid
+    <v-main>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
         >
-          <v-row
-            align="center"
-            justify="center"
-          >
-            <Login v-if="showLogin"></Login>
-            <Controlling v-if="user !== null"></Controlling>
-          </v-row>
-        </v-container>
-      </v-main>
+          <Login v-if="showLogin"></Login>
+          <Controlling v-if="user !== null"></Controlling>
+        </v-row>
+      </v-container>
+    </v-main>
 
-      <v-footer app>
-        <span>&copy; {{ new Date().getFullYear() }}</span>
-      </v-footer>
-    </v-app>
+    <v-footer app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
   </v-app>
 </template>
 <script>
